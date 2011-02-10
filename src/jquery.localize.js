a7fb6dfe20655bb44cbafe5a69b8c0ed9f5e79f1
grandecomplex <grandecomplex@gmail.com>
2011-02-09 21:54:53 -0800

    corrected whitespace

--- src/jquery.localize.js
+++ src/jquery.localize.js
@@ -66,13 +66,13 @@
         elem = $(this);
         key = elem.attr("rel").match(/localize\[(.*?)\]/)[1];
         value = valueForKey(key, data);
-        if (elem.is('input')) {
+		if (elem.is('input')) {
 			if (elem.attr("placeholder") != "") {
 				elem.attr("placeholder", value);
 			} else {
 				elem.val(value);
 			}
-        }
+		}
         if (elem.is('optgroup')) {
           elem.attr("label", value);
         }
